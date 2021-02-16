a73b9183b1063c6245e98d5a47e693326b4e04b3
daniel <danisimarroreigada@gmail.com>
2021-02-16 14:41:24 +0100

    Creacion de traductor

---
+++ src/main/java/Traductor/Traductor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Traductor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author daniel
+ */
+public class Traductor {
+    
+    private Map<String,String> traductor;
+
+    public Traductor() {
+        this.traductor= new HashMap<>();
+    }
+    
+    
+}
