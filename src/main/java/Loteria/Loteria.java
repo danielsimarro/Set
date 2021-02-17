5b4f587ce8cc005b6989f75b3d6168dbf93b2026
daniel <danisimarroreigada@gmail.com>
2021-02-17 14:39:34 +0100

    Creación de la loteria

--- src/main/java/Loteria/Loteria.java
+++ src/main/java/Loteria/Loteria.java
@@ -5,13 +5,64 @@
  */
 package Loteria;
 
+import java.util.ArrayList;
 import java.util.Map;
+
+import java.util.TreeMap;
 
 /**
  *
  * @author NitroPc
  */
 public class Loteria {
-    
-    private Map <String, Integer> loteria;
+
+    private Map<String, Integer> loteria;
+
+    public Loteria() {
+        this.loteria = new TreeMap<>();
+    }
+
+    //Introducción de valores 
+    public void introducirValores(String numero, Integer premio) {
+        loteria.put(numero, premio);
+    }
+
+    //Borar entrada según la clave
+    public void borrarEntradas(String numero) {
+        loteria.remove(numero);
+    }
+
+    //Modificar valores según la clave
+    public void modificarValores(String numero, Integer premio) {
+        loteria.replace(numero, premio);
+    }
+
+    //Obtener un atraducción según el la clave
+    public Integer obtenerPremio(String numero) {
+        return loteria.get(numero);
+    }
+
+    //Metodo para imprimir el MAP
+    public void imprimirMap() {
+        int contador=1;
+        for (String key : loteria.keySet()) {
+            System.out.println(contador + "- El número de loteria es --" + key + "-- con un premio de -->" + loteria.get(key) + "€");
+            contador++;
+        }
+    }
+
+    //Metodo keyset devuelve las claves del metodo
+    public ArrayList<String> listaTodasClaves() {
+        ArrayList<String> lista = new ArrayList<>(loteria.keySet());
+
+        return lista;
+    }
+
+    //El metodo values devuelve los valores sin las claves primarias
+    public ArrayList<Integer> listaPremios() {
+        ArrayList<Integer> lista = new ArrayList<>(loteria.values());
+
+        return lista;
+    }
+
 }
--- src/main/java/Traductor/Traductor.java
+++ src/main/java/Traductor/Traductor.java
@@ -5,7 +5,6 @@
  */
 package Traductor;
 
-
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,47 +14,53 @@ import java.util.TreeMap;
  * @author daniel
  */
 public class Traductor {
-    
-    private Map<String,String> traductor;
 
+    //Atributo de la clase
+    private Map<String, String> traductor;
+
+    //Constructor de la clase
     public Traductor() {
         this.traductor = new TreeMap<>();
     }
-    
-    public void introducirValores(String palabraIngles, String palabraEspañol){
+
+    //Introducción de valores 
+    public void introducirValores(String palabraIngles, String palabraEspañol) {
         traductor.put(palabraIngles, palabraEspañol);
     }
-    
-    public void borrarEntradas(String palabraIngles){
+
+    //Borar entrada según la clave
+    public void borrarEntradas(String palabraIngles) {
         traductor.remove(palabraIngles);
     }
-    
-    public void modificarValores(String palabraIngles, String palabraEspañol){
+
+    //Modificar valores según la clave
+    public void modificarValores(String palabraIngles, String palabraEspañol) {
         traductor.replace(palabraIngles, palabraEspañol);
     }
-    
-    public String obtenerTraduccion(String palabraIngles){
-       return traductor.get(palabraIngles);
+
+    //Obtener un atraducción según el la clave
+    public String obtenerTraduccion(String palabraIngles) {
+        return traductor.get(palabraIngles);
     }
-    
-    public void imprimirMap(){
-        for(String key : traductor.keySet()){
+
+    //Metodo para imprimir el MAP
+    public void imprimirMap() {
+        for (String key : traductor.keySet()) {
             System.out.println("La palabara en ingles es " + key + " que traducida al Español seria " + traductor.get(key));
         }
     }
-    
-    
+
     //Metodo keyset devuelve las claves del metodo
-    public ArrayList<String> listaTodasClaves(){
+    public ArrayList<String> listaTodasClaves() {
         ArrayList<String> lista = new ArrayList<>(traductor.keySet());
-       
+
         return lista;
     }
-    
+
     //El metodo values devuelve los valores sin las claves primarias
-    public ArrayList<String> listaPalabraEspañol(){
+    public ArrayList<String> listaPalabraEspañol() {
         ArrayList<String> lista = new ArrayList<>(traductor.values());
-        
+
         return lista;
     }
 }
